41e68a6b914998ed17769a5d5b1922e2f232b42c
Ahmet Faruk UNAL <ahmet@unalfaruk.com>
2018-05-12 14:27:48 +0300

    Basic of the purpose was completed.

--- mainCode.py
+++ mainCode.py
@@ -3,8 +3,8 @@ from allFunctions import *
 
 prepareDB()
 
-currentDT=datetime.datetime.now()
-print ("Today: %s\nYou can select a date in the next 10 days." % str(currentDT))
+currentDT=datetime.datetime.now().strftime("%d-%m-%Y")
+print ("Today: %s\nYou should select a date in the next 10 days." % str(currentDT))
 
 while 1:
     choosing=whishChoosing()
